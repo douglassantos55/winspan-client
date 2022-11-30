62b717478cef9693674e2152b9bd7585caabc473
Douglas Santos <hiperhavy_doug@hotmail.com>
2022-11-29 23:42:07 -0300

    feat: card styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { Bird } from "../types";
+import styles from "./Card.module.css";
 
 type Props = {
     bird: Bird;
@@ -8,8 +9,11 @@ type Props = {
 
 function Card({ bird, ...props }: Props) {
     return (
-        <div {...props}>
-            <h2>{bird.Name}</h2>
+        <div className={styles.container} {...props}>
+            <figure className={styles.imageContainer}>
+                <img src="http://placeimg.com/100/300/any" className={styles.image} alt={bird.Name} />
+            </figure>
+            <h2 className={styles.name}>{bird.Name}</h2>
         </div>
     );
 }
